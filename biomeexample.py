c88d325938c97df2539f514211d7adfab1c1f569
Federico Tomassetti <f.tomassetti@gmail.com>
2013-12-01 14:18:53 +0100

    Added generated worlds
    Former-commit-id: 7ddcbcddaf6c85d5ce358fd8175cb4e7305f57a8

--- biomeexample.py
+++
@@ -1,8 +0,0 @@
-from worldgen.geo import *
-from worldgen.draw import *
-	
-for i in xrange(10):
-	plates = generate_plates(i)
-	e = generate_base_heightmap(i,plates)
-	w = world_gen('World %i' % i,e,i)
-	draw_biome(w.biome,'biome_%i.png' % i)
