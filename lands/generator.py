e1ff659de7e70393bf4018d902e467497b832d40
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-17 18:54:21 +0100

    draw_oldmap returning image without saving it

--- lands/generator.py
+++ lands/generator.py
@@ -98,7 +98,7 @@ def check_step(step_name):
         return step
 
 def operation_ancient_map(world, map_filename):
-    img = draw.draw_oldmap(world, map_filename)
+    img = draw.draw_oldmap(world)
     img.save(map_filename)
     print("+ ancient map generated in '%s'" % map_filename)
 
