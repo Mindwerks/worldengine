a2ee280ee5fc6b9b2a6cd6e809f67ff2bf00e1c6
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-17 17:37:55 +0100

    Moving to version 0.2

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'world generator simulating plate tectonics, erosion, etc.',
-    'author': 'Federico Tomassetti',
-    'url': 'http://github.com/ftomassetti/lands',
-    'download_url': 'Where to download it.',
-    'author_email': 'f.tomassetti@gmail.com',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': ['lands'],
-    'scripts': [],
-    'name': 'lands'
+    'description'      : 'world generator simulating plate tectonics, erosion, etc.',
+    'author'           : 'Federico Tomassetti',
+    'url'              : 'http://github.com/ftomassetti/lands',
+    'download_url'     : 'http://github.com/ftomassetti/lands',
+    'author_email'     : 'f.tomassetti@gmail.com',
+    'version'          : '0.2',
+    'install_requires' : ['nose'],
+    'packages'         : ['lands'],
+    'scripts'          : [],
+    'name'             : 'lands'
 }
 
 setup(**config)
