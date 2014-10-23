8d01aa25e99b6f7af804f10680504c8260584f71
Federico Tomassetti <f.tomassetti@gmail.com>
2014-10-23 19:32:42 +0100

    Moving to version 0.3
    Former-commit-id: 8e520c2a112748466cb2de4153aff54e805325fc

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url'              : 'http://github.com/ftomassetti/lands',
     'download_url'     : 'http://github.com/ftomassetti/lands',
     'author_email'     : 'f.tomassetti@gmail.com',
-    'version'          : '0.2',
+    'version'          : '0.3',
     'install_requires' : ['nose'],
     'packages'         : ['lands'],
     'scripts'          : [],
