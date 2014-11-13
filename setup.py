b57e48d465700fa092112b055c8c39cff8546c52
Federico Tomassetti <f.tomassetti@gmail.com>
2014-11-13 19:56:38 +0000

    Adding a file to hold the version numbe
    Former-commit-id: eb72b64da6e311806615a22efe01b9a20ca6d848

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+execfile('lands/version.py')
+
 config = {
     'description'      : 'World generator simulating plate tectonics, erosion, etc.',
     'author'           : 'Federico Tomassetti',
     'url'              : 'http://github.com/ftomassetti/lands',
     'download_url'     : 'http://github.com/ftomassetti/lands',
     'author_email'     : 'f.tomassetti@gmail.com',
-    'version'          : '0.3.0',
+    'version'          : __version__,
     'install_requires' : ['nose'],
     'packages'         : ['lands'],
     'scripts'          : [],
