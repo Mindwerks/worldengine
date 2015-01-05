13d51833d768be9159cf9566546327fd014aaeb9
Federico Tomassetti <f.tomassetti@gmail.com>
2015-01-05 16:53:56 +0100

    Adding missing ui file
    Former-commit-id: 2df755e797eb507307187e2cdea4569d4a906f7e

---
+++ lands/gui.py
@@ -0,0 +1,10 @@
+from Tkinter import *
+# if you are working under Python 3, comment the previous line and comment out the following line
+#from tkinter import *
+
+root = Tk()
+
+w = Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
