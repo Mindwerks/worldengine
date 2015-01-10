7c1d26689866289761427501aa1dd90a26bb41aa
Federico Tomassetti <f.tomassetti@gmail.com>
2015-01-10 09:49:13 +0000

    gui: prepared function to show plates
    Former-commit-id: 791ae80539b6d7bfcaa95e0061fd5177ef21110f

--- lands/gui.py
+++ lands/gui.py
@@ -22,6 +22,29 @@ def prepare_menu():
 
     root.config(menu=menubar)
 
+def show_elevation_map(p, width, height):    
+    hm = platec.get_heightmap(p)
+    img = PhotoImage(width=canvas_width, height=canvas_height)
+    canvas.create_image(canvas_width/2,canvas_height/2, image=img, state="normal")
+    for y in xrange(height):
+        for x in xrange(width):
+            elev = hm[y*width+x]
+            if elev>0.5:
+                img.put("#00ff00",(x,y))
+            else:
+                img.put("#0000ff",(x,y))
+    
+def show_plates_map(p, width, height):    
+    pm = platec.get_platesmap(p)
+    colors = ["#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000",
+    "#990000","#aa0000","#bb0000"]
+    img = PhotoImage(width=canvas_width, height=canvas_height)
+    canvas.create_image(canvas_width/2,canvas_height/2, image=img, state="normal")
+    for y in xrange(height):
+        for x in xrange(width):
+            pi = pm[y*width+x]
+            img.put(colors[pi],(x,y))
+
 def file_new():
     print("Creating new map")
     seed=1
@@ -37,17 +60,10 @@ def file_new():
     p = platec.create(seed, width, height, sea_level, erosion_period, folding_ratio,
                       aggr_overlap_abs, aggr_overlap_rel, cycle_count, num_plates)
     print("Simulation")
-    hm = platec.get_heightmap(p)
-    img = PhotoImage(width=canvas_width, height=canvas_height)
-    canvas.create_image(canvas_width/2,canvas_height/2, image=img, state="normal")
-    for y in xrange(height):
-        for x in xrange(width):
-            elev = hm[y*width+x]
-            if elev>0.5:
-                img.put("#00ff00",(x,y))
-            else:
-                img.put("#0000ff",(x,y))
-    updateDisp()
+
+    show_elevation_map(p, width, height)
+
+    root.update_idletasks()
     print("Drawing")            
 
 def file_open():
