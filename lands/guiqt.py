c4d5857af610a389f1af7beeffbc4e11a666ac81
Federico Tomassetti <federico@tomassetti.me>
2015-01-31 11:34:09 +0000

    qt: generate dialog completed
    Former-commit-id: a0b90d3ce13f20f85d9d6fafc53cbef08f6acdc5

--- lands/guiqt.py
+++ lands/guiqt.py
@@ -7,6 +7,7 @@ GUI Interface for Lands
 
 import sys
 from PyQt4 import QtGui
+import random
 
 class GenerateDialog(QtGui.QDialog):
 
@@ -15,7 +16,62 @@ class GenerateDialog(QtGui.QDialog):
         self._init_ui()
 
     def _init_ui(self):            
-    	self.resize(500, 400)
+        self.resize(500, 300)
+        self.setWindowTitle('Generate a new world')
+        grid = QtGui.QGridLayout()
+
+        seed =  random.randint(0, 65535)
+
+        name_label = QtGui.QLabel('Name')
+        grid.addWidget(name_label, 0,0,1,1)
+        name = 'world_seed_%i' % seed 
+        name_value = QtGui.QLineEdit(name)
+        grid.addWidget(name_value, 0,1,1,2)
+
+        seed_label = QtGui.QLabel('Seed')
+        grid.addWidget(seed_label, 1,0,1,1)        
+        seed_value = self._spinner_box(0, 65525, seed)
+        grid.addWidget(seed_value, 1,1,1,2)
+
+        width_label = QtGui.QLabel('Width')
+        grid.addWidget(width_label, 2,0,1,1)        
+        width_value = self._spinner_box(100, 8192, 512)
+        grid.addWidget(width_value, 2,1,1,2)
+
+        height_label = QtGui.QLabel('Height')
+        grid.addWidget(height_label, 3,0,1,1)
+        height_value = self._spinner_box(100, 8192, 512)
+        grid.addWidget(height_value, 3,1,1,2)        
+
+        plates_num_label = QtGui.QLabel('Number of plates')
+        grid.addWidget(plates_num_label, 4,0,1,1)
+        plates_num_value = self._spinner_box(2, 100, 10)
+        grid.addWidget(plates_num_value, 4,1,1,2)
+
+        platesres_w_label = QtGui.QLabel('Plates resolution (width)')
+        grid.addWidget(platesres_w_label, 5,0,1,1)
+        platesres_w_value = self._spinner_box(50, 4096, 512)
+        grid.addWidget(platesres_w_value, 5,1,1,2)
+
+        platesres_h_label = QtGui.QLabel('Plates resolution (height)')
+        grid.addWidget(platesres_h_label, 6,0,1,1)
+        platesres_h_value = self._spinner_box(50, 4096, 512)
+        grid.addWidget(platesres_h_value, 6,1,1,2)
+
+        buttons_row = 7
+        cancel = QtGui.QPushButton('Cancel')
+        generate = QtGui.QPushButton('Generate')
+        grid.addWidget(cancel,   buttons_row, 1, 1, 1)
+        grid.addWidget(generate, buttons_row, 2, 1, 1)
+
+        self.setLayout(grid)
+
+    def _spinner_box(self, min, max, value):
+        spinner = QtGui.QSpinBox()
+        spinner.setMinimum(min)
+        spinner.setMaximum(max)
+        spinner.setValue(value)
+        return spinner
 
 class LandsGui(QtGui.QMainWindow):
     
@@ -24,22 +80,22 @@ class LandsGui(QtGui.QMainWindow):
         self._init_ui()
         
     def set_status(self, message):
-    	self.statusBar().showMessage(message)
+        self.statusBar().showMessage(message)
         
     def _init_ui(self):            
-    	self.resize(800, 600)
-    	self.setWindowTitle('Lands - A world generator')        
-    	self.set_status('No world selected: create or load a world')
-    	self._prepare_menu()
+        self.resize(800, 600)
+        self.setWindowTitle('Lands - A world generator')        
+        self.set_status('No world selected: create or load a world')
+        self._prepare_menu()
         self.show()
 
     def _prepare_menu(self):
-    	generateAction = QtGui.QAction('&Generate', self)        
+        generateAction = QtGui.QAction('&Generate', self)        
         generateAction.setShortcut('Ctrl+G')
         generateAction.setStatusTip('Generate new world')
         generateAction.triggered.connect(self._on_generate)
 
-    	exitAction = QtGui.QAction('&Exit', self)        
+        exitAction = QtGui.QAction('&Exit', self)        
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(QtGui.qApp.quit)
@@ -50,9 +106,9 @@ class LandsGui(QtGui.QMainWindow):
         fileMenu.addAction(exitAction)
 
     def _on_generate(self):
-    	print("Generate...")
-    	dialog = GenerateDialog()
-    	dialog.exec_()
+        print("Generate...")
+        dialog = GenerateDialog()
+        dialog.exec_()
         
 
 def main():
