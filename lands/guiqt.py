59c6a58d66e0c6806b156096b04ed34f30ae08f7
Federico Tomassetti <federico@tomassetti.me>
2015-01-31 14:52:16 +0000

    qt: generation properly executed

--- lands/guiqt.py
+++ lands/guiqt.py
@@ -9,6 +9,7 @@ import sys
 from PyQt4 import QtGui
 import random
 import threading
+import platec
 
 class GenerateDialog(QtGui.QDialog):
 
@@ -26,38 +27,38 @@ class GenerateDialog(QtGui.QDialog):
         name_label = QtGui.QLabel('Name')
         grid.addWidget(name_label, 0,0,1,1)
         name = 'world_seed_%i' % seed 
-        name_value = QtGui.QLineEdit(name)
-        grid.addWidget(name_value, 0,1,1,2)
+        self.name_value = QtGui.QLineEdit(name)
+        grid.addWidget(self.name_value, 0,1,1,2)
 
         seed_label = QtGui.QLabel('Seed')
         grid.addWidget(seed_label, 1,0,1,1)        
-        seed_value = self._spinner_box(0, 65525, seed)
-        grid.addWidget(seed_value, 1,1,1,2)
+        self.seed_value = self._spinner_box(0, 65525, seed)
+        grid.addWidget(self.seed_value, 1,1,1,2)
 
         width_label = QtGui.QLabel('Width')
         grid.addWidget(width_label, 2,0,1,1)        
-        width_value = self._spinner_box(100, 8192, 512)
-        grid.addWidget(width_value, 2,1,1,2)
+        self.width_value = self._spinner_box(100, 8192, 512)
+        grid.addWidget(self.width_value, 2,1,1,2)
 
         height_label = QtGui.QLabel('Height')
         grid.addWidget(height_label, 3,0,1,1)
-        height_value = self._spinner_box(100, 8192, 512)
-        grid.addWidget(height_value, 3,1,1,2)        
+        self.height_value = self._spinner_box(100, 8192, 512)
+        grid.addWidget(self.height_value, 3,1,1,2)        
 
         plates_num_label = QtGui.QLabel('Number of plates')
         grid.addWidget(plates_num_label, 4,0,1,1)
-        plates_num_value = self._spinner_box(2, 100, 10)
-        grid.addWidget(plates_num_value, 4,1,1,2)
+        self.plates_num_value = self._spinner_box(2, 100, 10)
+        grid.addWidget(self.plates_num_value, 4,1,1,2)
 
         platesres_w_label = QtGui.QLabel('Plates resolution (width)')
         grid.addWidget(platesres_w_label, 5,0,1,1)
-        platesres_w_value = self._spinner_box(50, 4096, 512)
-        grid.addWidget(platesres_w_value, 5,1,1,2)
+        self.platesres_w_value = self._spinner_box(50, 4096, 512)
+        grid.addWidget(self.platesres_w_value, 5,1,1,2)
 
         platesres_h_label = QtGui.QLabel('Plates resolution (height)')
         grid.addWidget(platesres_h_label, 6,0,1,1)
-        platesres_h_value = self._spinner_box(50, 4096, 512)
-        grid.addWidget(platesres_h_value, 6,1,1,2)
+        self.platesres_h_value = self._spinner_box(50, 4096, 512)
+        grid.addWidget(self.platesres_h_value, 6,1,1,2)
 
         buttons_row = 7
         cancel   = QtGui.QPushButton('Cancel')
@@ -82,50 +83,89 @@ class GenerateDialog(QtGui.QDialog):
     def _on_generate(self):        
         QtGui.QDialog.accept(self)
 
+    def seed(self):
+        return self.seed_value.value()
+
+    def width(self):
+        return self.platesres_w_value.value()
+
+    def height(self):
+        return self.platesres_h_value.value()
+
+    def num_plates(self):        
+        return self.plates_num_value.value()
+
 class GenerationProgressDialog(QtGui.QDialog):
 
-    def __init__(self,parent=None):
-        QtGui.QDialog.__init__(self,parent)
+    def __init__(self, seed, width, height, num_plates):
+        QtGui.QDialog.__init__(self)
         self._init_ui()
+        self.gen_thread = GenerationThread(self, seed, width, height, num_plates)
+        self.gen_thread.start()
 
     def _init_ui(self):            
         self.resize(400, 100)
         self.setWindowTitle('Generating a new world...')
         grid = QtGui.QGridLayout()
 
-        status = QtGui.QLabel('....') 
-        grid.addWidget(status, 0, 0, 1, 3)       
-
-        progress = QtGui.QProgressBar()
-        progress.setMinimum(0)
-        progress.setMaximum(100)
-        progress.setValue(0)
-        grid.addWidget(progress, 1, 0, 1, 3)       
+        self.status = QtGui.QLabel('....') 
+        grid.addWidget(self.status, 0, 0, 1, 3)          
 
         cancel   = QtGui.QPushButton('Cancel')
-        grid.addWidget(cancel, 2, 0, 1, 1)
+        grid.addWidget(cancel, 1, 0, 1, 1)
         cancel.clicked.connect(self._on_cancel)
 
         self.setLayout(grid)
 
     def _on_cancel(self):
-        QtGui.QDialog.reject(self)        
+        QtGui.QDialog.reject(self)       
+
+    def set_status(self, message):
+        self.status.setText(message)
 
 
 class GenerationThread(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, ui, seed, width, height, num_plates):
         threading.Thread.__init__(self)
+        print('seed %i' % seed)
+        print('width %i' % width)
+        print('height %i' % height)
+        print('num_plates %i' % num_plates)
+        self.plates_generation = PlatesGeneration(seed, width, height, num_plates=num_plates)
+        self.ui = ui
     
-    def run (self):
-        print("thread")        
+    def run (self):        
+        finished = False
+        while not finished:
+            (finished, n_steps) = self.plates_generation.step() 
+            print("n_steps %i %s" % (n_steps, finished))       
+            self.ui.set_status('Step %i' % n_steps)        
+
+class PlatesGeneration():
+
+    def __init__(self, seed, width, height, 
+                 sea_level=0.65, erosion_period=60,
+                 folding_ratio=0.02, aggr_overlap_abs=1000000, aggr_overlap_rel=0.33,
+                 cycle_count=2, num_plates=10):
+        self.p = platec.create(seed, width, height, sea_level, erosion_period, folding_ratio,
+                               aggr_overlap_abs, aggr_overlap_rel, cycle_count, num_plates)
+        self.steps = 0
+
+    def step(self):
+        if platec.is_finished(self.p) == 0:
+            platec.step(self.p)
+            self.steps += 1
+            return (False, self.steps)
+        else:
+            return (True, self.steps)            
 
 class LandsGui(QtGui.QMainWindow):
     
     def __init__(self):
         super(LandsGui, self).__init__()        
         self._init_ui()
-        
+
     def set_status(self, message):
         self.statusBar().showMessage(message)
         
@@ -155,10 +195,12 @@ class LandsGui(QtGui.QMainWindow):
     def _on_generate(self):
         dialog = GenerateDialog()
         ok = dialog.exec_()
-        if ok:
-            gen_thread = GenerationThread()
-            dialog2 = GenerationProgressDialog()
-            gen_thread.start()
+        if ok:            
+            seed = dialog.seed()
+            width = dialog.width()
+            height = dialog.height()
+            num_plates = dialog.num_plates()
+            dialog2 = GenerationProgressDialog(seed, width, height, num_plates)            
             ok2     = dialog2.exec_()         
 
 def main():
