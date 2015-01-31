313d5a78e310b4d9a836549df8abe9fdd96c7dd2
Federico Tomassetti <federico@tomassetti.me>
2015-01-31 10:37:12 +0000

    qt: adding a dialog
    Former-commit-id: 54b0edc954abe57830af204043a38ea7135df853

--- lands/guiqt.py
+++ lands/guiqt.py
@@ -8,23 +8,30 @@ GUI Interface for Lands
 import sys
 from PyQt4 import QtGui
 
+class GenerateDialog(QtGui.QDialog):
+
+    def __init__(self,parent=None):
+        QtGui.QDialog.__init__(self,parent)
+        self._init_ui()
+
+    def _init_ui(self):            
+    	self.resize(500, 400)
+
 class LandsGui(QtGui.QMainWindow):
     
     def __init__(self):
-        super(LandsGui, self).__init__()
-        
-        self.initUI()
+        super(LandsGui, self).__init__()        
+        self._init_ui()
         
     def set_status(self, message):
     	self.statusBar().showMessage(message)
         
-    def initUI(self):            
+    def _init_ui(self):            
     	self.resize(800, 600)
     	self.setWindowTitle('Lands - A world generator')        
     	self.set_status('No world selected: create or load a world')
     	self._prepare_menu()
         self.show()
-
 
     def _prepare_menu(self):
     	generateAction = QtGui.QAction('&Generate', self)        
@@ -44,6 +51,8 @@ class LandsGui(QtGui.QMainWindow):
 
     def _on_generate(self):
     	print("Generate...")
+    	dialog = GenerateDialog()
+    	dialog.exec_()
         
 
 def main():
