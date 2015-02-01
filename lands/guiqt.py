7b92d8c6785ff3cfcd537a605a9e502c54fd952e
Federico Tomassetti <federico@tomassetti.me>
2015-02-01 16:34:05 +0000

    qt: correcting drawing in bw

--- lands/guiqt.py
+++ lands/guiqt.py
@@ -20,7 +20,22 @@ def _draw_simple_elevation_on_screen(world, canvas):
         for x in range(0, width):
             e = world.elevation['data'][y][x]
             r, g, b = elevation_color(e)
-            canvas.setPixel(x, y, int(r * 255 * 255 * 255) + int(g * 255 * 255) + int(b * 255))    
+            col = QtGui.QColor(int(r*255),int(g*255),int(b*255))
+            canvas.setPixel(x, y, col.rgb())    
+
+def _draw_bw_elevation_on_screen(world, canvas):
+    width = world.width
+    height = world.height
+    max_el = world.max_elevation()
+    min_el = world.min_elevation()
+    delta_el = max_el - min_el
+    for y in range(0, height):
+        for x in range(0, width):
+            e = world.elevation['data'][y][x]
+            e_normalized = (e - min_el) / delta_el
+            r = g = b = e_normalized
+            col = QtGui.QColor(int(r*255),int(g*255),int(b*255))             
+            canvas.setPixel(x, y, col.rgb())
 
 class GenerateDialog(QtGui.QDialog):
 
@@ -212,7 +227,8 @@ class MapCanvas(QtGui.QImage):
         self._update()
 
     def draw_world(self, world):
-        _draw_simple_elevation_on_screen(world, self)
+        #_draw_simple_elevation_on_screen(world, self)
+        _draw_bw_elevation_on_screen(world, self)
         self._update()
 
     def _update(self):
