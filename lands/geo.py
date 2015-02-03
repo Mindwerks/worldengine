dd296851eed8f698bbbde533a65cd153effe4df9
Federico Tomassetti <federico@tomassetti.me>
2015-02-03 21:55:18 +0000

    Fixing import mechanism

--- lands/geo.py
+++ lands/geo.py
@@ -1,11 +1,7 @@
 __author__ = 'Federico Tomassetti'
 
 from noise import snoise2
-
-try:
-    from lands.world import *
-except:
-    from world import *
+from lands.world import *
 
 import sys
 if sys.version_info > (2,):
