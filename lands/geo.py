d7403086c50834706609d538d65a36b82b2b24c8
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-10 11:12:51 +0000

    nitpicking on lands/geo.py

--- lands/geo.py
+++ lands/geo.py
@@ -2,8 +2,15 @@ __author__ = 'Federico Tomassetti'
 
 import time
 from noise import snoise2
-from world import *
-from simulations import *
+from lands.world import *
+from lands.simulations.WatermapSimulation import *
+from lands.simulations.IrrigationSimulation import *
+from lands.simulations.HumiditySimulation import *
+from lands.simulations.TemperatureSimulation import *
+from lands.simulations.PermeabilitySimulation import *
+from lands.simulations.ErosionSimulation import *
+from lands.simulations.BiomeSimulation import *
+from lands.simulations.basic import *
 
 import sys
 if sys.version_info > (2,):
@@ -391,27 +398,6 @@ def place_oceans_at_map_borders(elevation):
         for i in range(0, OCEAN_BORDER):
             place_ocean(x, i, i)
             place_ocean(x, height - i - 1, i)
-
-
-def humidity(world):
-    humidity = {}
-    humidity['data'] = [[0 for x in xrange(world.width)] for y in xrange(world.height)]
-
-    for y in xrange(world.height):
-        for x in xrange(world.width):
-            humidity['data'][y][x] = world.precipitation['data'][y][x] + world.irrigation[y][x]
-
-    #These were originally evenly spaced at 12.5% each but changing them to a bell curve produced
-    #better results
-    humidity['quantiles'] = {}
-    humidity['quantiles']['12'] = find_threshold_f(humidity['data'], 0.02, world.ocean)
-    humidity['quantiles']['25'] = find_threshold_f(humidity['data'], 0.09, world.ocean)
-    humidity['quantiles']['37'] = find_threshold_f(humidity['data'], 0.26, world.ocean)
-    humidity['quantiles']['50'] = find_threshold_f(humidity['data'], 0.50, world.ocean)
-    humidity['quantiles']['62'] = find_threshold_f(humidity['data'], 0.74, world.ocean)
-    humidity['quantiles']['75'] = find_threshold_f(humidity['data'], 0.91, world.ocean)
-    humidity['quantiles']['87'] = find_threshold_f(humidity['data'], 0.98, world.ocean)
-    return humidity
 
 
 def initialize_ocean_and_thresholds(world, ocean_level=1.0):
