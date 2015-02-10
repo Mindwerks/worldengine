a96c4ce25324e5748751ed996207a1c4024c385d
Federico Tomassetti <federico@tomassetti.me>
2015-02-10 19:12:28 +0000

    setup: listing dependencies
    Former-commit-id: 759189a74c88138c4d40455d11641bae44090ea7

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, Extension, Command
+#from pip.req import parse_requirements
+#import pip.download
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+#install_reqs = parse_requirements("requirements2.txt", session=pip.download.PipSession())
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+#reqs = [str(ir.req) for ir in install_reqs]
 
 execfile('lands/version.py')
 
@@ -14,6 +23,7 @@ config = {
     'entry_points'     : {
         'console_scripts': ['lands=lands.generator:main', 'landsgui=lands.gui.main:main'],
     },
+    'install_requires':  ['Pillow==2.6.1', 'PyPlatec==1.2.8', 'langgen==0.1.2', 'argparse==1.2.1', 'noise==1.2.1', 'nose==1.3.1', 'wsgiref==0.1.2', 'protobuf==2.6.0'],
     'name'             : 'lands'
 }
 
