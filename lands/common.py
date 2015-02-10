890fa98cfd5218b10246923977bf17a87c19de34
Federico Tomassetti <federico@tomassetti.me>
2015-02-10 22:41:08 +0000

    revising lands/common

--- lands/common.py
+++ lands/common.py
@@ -1,12 +1,19 @@
 __author__ = 'Federico Tomassetti'
 
+verbose = False
+
+
 def get_verbose():
     global verbose
-    if not 'verbose' in globals():
+    if 'verbose' not in globals():
         return False
     else:
         return verbose
 
+
 def set_verbose(value):
+    """
+    Set the level of verbosity for all the operations executed in Lands
+    """
     global verbose
     verbose = value
