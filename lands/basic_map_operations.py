6aad9355d380d73f5235d5f8acee2db7c8b04012
Federico Tomassetti <federico@tomassetti.me>
2015-02-10 23:07:05 +0000

    test: testing index_of_nearest

--- lands/basic_map_operations.py
+++ lands/basic_map_operations.py
@@ -14,12 +14,17 @@ def distance(pa, pb):
     return math.sqrt((ax - bx) ** 2 + (ay - by) ** 2)
 
 
-def nearest(p, hot_points, distance_f=distance):
+def index_of_nearest(p, hot_points, distance_f=distance):
+    """Given a point and a set of hot points it found the hot point
+    nearest to the given point. An arbitrary distance function can
+    be specified
+    :return the index of the nearest hot points, or None if the list of hot points is empty
+    """
     min_dist = None
     nearest_hp_i = None
     for i, hp in enumerate(hot_points):
         dist = distance_f(p, hp)
-        if (None == min_dist) or dist < min_dist:
+        if min_dist is None or dist < min_dist:
             min_dist = dist
             nearest_hp_i = i
     return nearest_hp_i
--- tests/basic_map_operations_test.py
+++ tests/basic_map_operations_test.py
@@ -25,5 +25,12 @@ class TestBasicMapOperations(unittest.TestCase):
         self.assertAlmostEquals(22.360679774997898, distance((-1, 9), (9, 29)))
         self.assertAlmostEquals(22.360679774997898, distance((9, -1), (19, 19)))
 
+    def test_index_of_nearest(self):
+        self.assertIsNone(index_of_nearest((0, 0), []))
+        self.assertEqual(0, index_of_nearest((0, 0), [(0, 0), (10, 10), (7, 7), (-5, -5), (-2, 7)]))
+        self.assertEqual(3, index_of_nearest((-4, -4), [(0, 0), (10, 10), (7, 7), (-5, -5), (-2, 7)]))
+        self.assertEqual(3, index_of_nearest((-100, -100), [(0, 0), (10, 10), (7, 7), (-5, -5), (-2, 7)]))
+        self.assertEqual(3, index_of_nearest((-100.0, -100.0), [(0, 0), (10, 10), (7, 7), (-5, -5), (-2, 7)]))
+
 if __name__ == '__main__':
     unittest.main()
