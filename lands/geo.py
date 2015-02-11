304ab3c43626685ea68ca60eef478a4a40b645be
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-11 16:25:01 +0000

    moving scaling functions to common

--- lands/geo.py
+++ lands/geo.py
@@ -138,93 +138,6 @@ def initialize_ocean_and_thresholds(world, ocean_level=1.0):
 
 
 # -------
-# Scaling
-# -------
-
-def scale(original_map, target_width, target_height):
-
-    def _get_interleave_value(original_map, x, y):
-        """x and y can be float value"""
-
-        weight_next_x, base_x = math.modf(x)
-        weight_preceding_x = 1.0 - weight_next_x
-        weight_next_y, base_y = math.modf(y)
-        weight_preceding_y = 1.0 - weight_next_y
-
-        base_x = int(base_x)
-        base_y = int(base_y)
-
-        sum = 0.0
-
-        # In case the point is right on the border, the weight
-        # of the next point will be zero and we will not access
-        # it
-        combined_weight = weight_preceding_x * weight_preceding_y
-        if combined_weight > 0.0:
-            sum += combined_weight * original_map[base_y][base_x]
-
-        combined_weight = weight_preceding_x * weight_next_y
-        if combined_weight > 0.0:
-            sum += combined_weight * original_map[base_y + 1][base_x]
-
-        combined_weight = weight_next_x * weight_preceding_y
-        if combined_weight > 0.0:
-            sum += combined_weight * original_map[base_y][base_x + 1]
-
-        combined_weight = weight_next_x * weight_next_y
-        if combined_weight > 0.0:
-            sum += combined_weight * original_map[base_y + 1][base_x + 1]
-
-        return sum
-
-    original_width = len(original_map[0])
-    original_height = len(original_map)
-
-    y_factor = float(original_height - 1) / (target_height - 1)
-    x_factor = float(original_width - 1) / (target_width - 1)
-
-    scaled_map = [[0 for x in range(target_width)] for y in range(target_height)]
-    for scaled_y in range(target_height):
-        original_y = y_factor * scaled_y
-        for scaled_x in range(target_width):
-            original_x = x_factor * scaled_x
-            scaled_map[scaled_y][scaled_x] = _get_interleave_value(original_map, original_x, original_y)
-
-    return scaled_map
-
-
-def antialias(elevation, steps):
-    width = len(elevation[0])
-    height = len(elevation)
-
-    def _antialias_step():
-        for y in range(height):
-            for x in range(width):
-                antialias_point(x, y)
-
-    def antialias_point(x, y):
-        n = 2
-        tot = elevation[y][x] * 2
-        for dy in range(-1, +2):
-            py = y + dy
-            if py > 0 and py < height:
-                for dx in range(-1, +2):
-                    px = x + dx
-                    if px > 0 and px < width:
-                        n += 1
-                        tot += elevation[py][px]
-        return tot / n
-
-    for i in range(steps):
-        _antialias_step()
-
-
-def rescale_value(original, prev_min, prev_max, min, max):
-    f = float(original - prev_min) / (prev_max - prev_min)
-    return min + ((max - min) * f)
-
-
-# -------
 # Misc
 # ----
 
@@ -261,7 +174,7 @@ def _around(x, y, width, height):
     return ps
 
 
-def world_gen_from_elevation(w, step):
+def generate_world(w, step):
     if isinstance(step, str):
         step = Step.get_by_name(step)
     seed = w.seed
