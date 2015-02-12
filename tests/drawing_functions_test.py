03b86f7738043af2aeec7839f5f03d67042982a3
Federico Tomassetti <federico@tomassetti.me>
2015-02-12 19:02:35 +0000

    test: fixing test for ancientmap
    Former-commit-id: 21ca6beff614c3cfbbd10d1ea1065efdfd51893d

--- tests/drawing_functions_test.py
+++ tests/drawing_functions_test.py
@@ -1,24 +1,28 @@
 __author__ = 'Federico Tomassetti'
 
 import unittest
-import os
-from PIL import Image
-from lands.draw import *
-from lands.draw import _biome_colors # need to be explicitly imported
+
+from lands.drawing_functions import *
 from lands.world import *
-from tests.draw_test import TestDraw, PixelCollector
+from tests.draw_test import TestBase, PixelCollector, TestDraw
 
 
-class TestDrawingFunctions(TestDraw):
+class TestDrawingFunctions(TestBase):
 
     def setUp(self):
-        TestDraw.setUp(self)
+        super(TestDrawingFunctions, self).setUp()
 
-    def test_draw_ancientmap(self):
+    def test_draw_ancientmap_factor1(self):
+        w_large = World.from_pickle_file("%s/seed_48956.world" % self.tests_data_dir)
+        target = PixelCollector(w_large.width, w_large.height)
+        draw_ancientmap(w_large, target, resize_factor=1)
+        self._assert_img_equal("ancientmap_48956", target)
+
+    def test_draw_ancientmap_factor3(self):
         w = World.open_protobuf("%s/seed_28070.world" % self.tests_data_dir)
-        target = PixelCollector(w.width, w.height)
-        draw_biome(w, target)
-        self._assert_img_equal("biome_28070", target)
+        target = PixelCollector(w.width * 3, w.height * 3)
+        draw_ancientmap(w, target, resize_factor=3)
+        self._assert_img_equal("ancientmap_28070_factor3", target)
 
 if __name__ == '__main__':
     unittest.main()
