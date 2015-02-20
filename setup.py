db046a16cacab75324606b97e40d5963060ea4f7
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-20 11:10:44 +0000

    using pyplatec 1.2.9
    Former-commit-id: 27a23dae0edb8723a056699d03c6b1abd02c5fc8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ config = {
     'entry_points'     : {
         'console_scripts': ['lands=lands.cli.main:main', 'landsgui=lands.gui.main:main'],
     },
-    'install_requires':  ['Pillow==2.6.1', 'PyPlatec==1.2.8', 'langgen==0.1.2', 'argparse==1.2.1', 'noise==1.2.1', 'nose==1.3.1', 'wsgiref==0.1.2', 'protobuf==2.6.0'],
+    'install_requires':  ['Pillow==2.6.1', 'PyPlatec==1.2.9', 'langgen==0.1.2', 'argparse==1.2.1', 'noise==1.2.1', 'nose==1.3.1', 'wsgiref==0.1.2', 'protobuf==2.6.0'],
     'name'             : 'lands'
 }
 
