fa74e884431b0de5e3ef415c98cf591d44e6c23f
Federico Tomassetti <ftomassetti@groupon.com>
2015-02-24 15:55:15 +0000

    pep8: gui/view
    Former-commit-id: 42a2cb6f3f1197a0595bee91f9dd2fd4746e35cd

--- worldengine/gui/view.py
+++ worldengine/gui/view.py
@@ -2,6 +2,7 @@ from PyQt4 import QtGui
 from worldengine.draw import elevation_color
 import worldengine.drawing_functions
 
+
 def draw_simple_elevation_on_screen(world, canvas):
     width = world.width
     height = world.height
@@ -9,7 +10,7 @@ def draw_simple_elevation_on_screen(world, canvas):
         for x in range(0, width):
             e = world.elevation['data'][y][x]
             r, g, b = elevation_color(e)
-            col = QtGui.QColor(int(r*255),int(g*255),int(b*255))
+            col = QtGui.QColor(int(r * 255), int(g * 255), int(b * 255))
             canvas.setPixel(x, y, col.rgb())
 
 
@@ -24,13 +25,14 @@ def draw_bw_elevation_on_screen(world, canvas):
             e = world.elevation['data'][y][x]
             e_normalized = (e - min_el) / delta_el
             r = g = b = e_normalized
-            col = QtGui.QColor(int(r*255),int(g*255),int(b*255))
+            col = QtGui.QColor(int(r * 255), int(g * 255), int(b * 255))
             canvas.setPixel(x, y, col.rgb())
 
 
 def cos(x):
     import math
-    return math.cos(x/180 * 3.14)
+
+    return math.cos(x / 180 * 3.14)
 
 
 def hsi_to_rgb(hue, saturation, intensity):
@@ -39,13 +41,13 @@ def hsi_to_rgb(hue, saturation, intensity):
     I = intensity
     IS = saturation * intensity
 
-    if h==0:
+    if h == 0:
         R = I + 2 * IS
         G = I - IS
         B = I - IS
     elif 0 < H and H < 120:
-        R = I + IS*cos(H)/cos(60-H)
-        G = I + IS*(1 - cos(H)/cos(60-H))
+        R = I + IS * cos(H) / cos(60 - H)
+        G = I + IS * (1 - cos(H) / cos(60 - H))
         B = I - IS
     elif H == 120:
         R = I - IS
@@ -53,16 +55,16 @@ def hsi_to_rgb(hue, saturation, intensity):
         B = I - IS
     elif 120 < H and H < 240:
         R = I - IS
-        G = I + ((IS*cos(H-120))/cos(180-H))
-        B = I + IS*(1 - cos(H-120)/cos(180-H))
+        G = I + ((IS * cos(H - 120)) / cos(180 - H))
+        B = I + IS * (1 - cos(H - 120) / cos(180 - H))
     elif H == 240:
         R = I - IS
         G = I - IS
         B = I + 2 * IS
     elif 240 < H and H < 360:
-        R = I + IS*(1 - cos(H-240)/cos(300-H))
+        R = I + IS * (1 - cos(H - 240) / cos(300 - H))
         G = I - IS
-        B = I + IS*cos(H-240)/cos(300-H)
+        B = I + IS * cos(H - 240) / cos(300 - H)
     else:
         raise "Unexpected"
     return (int(R), int(G), int(B))
@@ -74,7 +76,7 @@ def draw_plates_on_screen(world, canvas):
     n_plates = world.n_actual_plates()
     for y in range(0, height):
         for x in range(0, width):
-            h = world.plates[y][x]*(360/n_plates)
+            h = world.plates[y][x] * (360 / n_plates)
             s = 0.5
             i = 64.0
             r, g, b = hsi_to_rgb(h, s, i)
@@ -91,10 +93,10 @@ def draw_plates_and_elevation_on_screen(world, canvas):
     delta_el = max_el - min_el
     for y in range(0, height):
         for x in range(0, width):
-            h = world.plates[y][x]*(360/n_plates)
+            h = world.plates[y][x] * (360 / n_plates)
             el = world.elevation['data'][y][x]
             s = 0.6
-            i = 40.0 + 60.0 * ((el-min_el)/delta_el)
+            i = 40.0 + 60.0 * ((el - min_el) / delta_el)
             r, g, b = hsi_to_rgb(h, s, i)
             col = QtGui.QColor(r, g, b)
             canvas.setPixel(x, y, col.rgb())
@@ -107,7 +109,7 @@ def draw_land_on_screen(world, canvas):
     ocean_color = QtGui.QColor(0, 0, 200).rgb()
     for y in range(0, height):
         for x in range(0, width):
-            if world.is_land((x,y)):
+            if world.is_land((x, y)):
                 canvas.setPixel(x, y, land_color)
             else:
                 canvas.setPixel(x, y, ocean_color)
