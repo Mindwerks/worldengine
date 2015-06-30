1d563ebf9a187d1d3b78584afbafe98c79ef6083
Federico Tomassetti <f.tomassetti@gmail.com>
2015-06-30 15:34:01 +0200

    Improve ancient map operation

--- worldengine/draw.py
+++ worldengine/draw.py
@@ -431,8 +431,12 @@ def draw_biome_on_file(world, filename):
 
 
 def draw_ancientmap_on_file(world, filename, resize_factor=1,
-                            sea_color=(212, 198, 169, 255), verbose=False):
+                            sea_color=(212, 198, 169, 255),
+                            draw_biome=True, draw_rivers=True, draw_mountains=True, 
+                            draw_outer_land_border=False, verbose=False):
     img = ImagePixelSetter(world.width * resize_factor,
                            world.height * resize_factor, filename)
-    draw_ancientmap(world, img, resize_factor, sea_color, verbose)
+    draw_ancientmap(world, img, resize_factor, sea_color,
+                    draw_biome, draw_rivers, draw_mountains, draw_outer_land_border, 
+                    verbose)
     img.complete()
