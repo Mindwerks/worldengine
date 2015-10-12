f9d53f9a226990a74d55f407a8aeebbcd6652ec3
tcl <theclaw.tcl@gmail.com>
2015-10-12 16:57:08 +0200

    Parameter ocean_level was not passed to platec.

--- worldengine/plates.py
+++ worldengine/plates.py
@@ -11,7 +11,7 @@ from worldengine.common import array_to_matrix
 from world import World
 
 
-def generate_plates_simulation(seed, width, height, sea_level=0.65,
+def generate_plates_simulation(seed, width, height, ocean_level=1.0,
                                erosion_period=60, folding_ratio=0.02,
                                aggr_overlap_abs=1000000, aggr_overlap_rel=0.33,
                                cycle_count=2, num_plates=10,
@@ -19,7 +19,7 @@ def generate_plates_simulation(seed, width, height, sea_level=0.65,
 
     if verbose:
         start_time = time.time()
-    p = platec.create(seed, width, height, sea_level, erosion_period,
+    p = platec.create(seed, width, height, ocean_level, erosion_period,
                       folding_ratio, aggr_overlap_abs, aggr_overlap_rel,
                       cycle_count, num_plates)
 
@@ -39,6 +39,7 @@ def _plates_simulation(name, width, height, seed, num_plates=10,
                        ocean_level=1.0, step=Step.full(),
                        verbose=get_verbose()):
     e_as_array, p_as_array = generate_plates_simulation(seed, width, height,
+                                                        ocean_level=ocean_level,
                                                         num_plates=num_plates,
                                                         verbose=verbose)
 
