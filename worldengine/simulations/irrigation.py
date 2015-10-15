072c9d9aa9d68b9ab4f9e8c35b630b341f550ca3
tcl <theclaw.tcl@gmail.com>
2015-10-15 11:56:53 +0200

    More refinements.

--- worldengine/simulations/irrigation.py
+++ worldengine/simulations/irrigation.py
@@ -12,43 +12,40 @@ class IrrigationSimulation(object):
     def _calculate(world):
         #Notes on performance:
         #  -method is run once per generation
-        #  -iterations        : width * height * (2 * radius + 1)^2
+        #  -iterations        : width * height
         #  -memory consumption: width * height * 8 Byte (numpy 1.9.2)
 
         width = world.width
         height = world.height
         radius = 10
 
-        #----------prepare helper arrays
-        #create array of pre-calculated values -> less calls to sqrt(), square() and log()
+        #create array of pre-calculated values -> less calculations
         d = numpy.arange(-radius, radius + 1, 1, dtype=numpy.float)
-        x, y = numpy.meshgrid(d, d)#x,y now contain x/y-coordinates of the distance-matrix
-        
-        #calculat the final matrix: ln(sqrt(x^2+y^2) + 1)
-        logs = numpy.log1p(numpy.sqrt(numpy.add(numpy.square(x), numpy.square(y))))
-        logs = numpy.add(logs, 1)
-        #----------preparations done
+        x, y = numpy.meshgrid(d, d)#x/y distances to array center
+        #calculate final matrix: ln(sqrt(x^2+y^2) + 1) + 1
+        logs = numpy.log1p(numpy.sqrt(numpy.square(x) + numpy.square(y))) + 1
 
         #create output array
         values = numpy.zeros((height, width), dtype=numpy.float)
-
+        
         it_all = numpy.nditer(values, flags=['multi_index'], op_flags=['readonly'])
-        it_logs = numpy.nditer(logs, flags=['multi_index'], op_flags=['readonly'])
         while not it_all.finished:
             x = it_all.multi_index[1]
             y = it_all.multi_index[0]
             if world.is_land((x, y)):
-                #iterate over area [[x-radius, x+radius], [y-radius, y+radius]]
-                it_logs.reset()
-                while not it_logs.finished:
-                    #calculate absolute pts; (y, x)
-                    pt = (y + it_logs.multi_index[0] - radius,
-                                  x + it_logs.multi_index[1] - radius)
+                #coordinates used for the values-slice (tl = top-left etc.)
+                tl_v = (max(x - radius, 0)        , max(y - radius, 0))
+                br_v = (min(x + radius, width - 1), min(y + radius, height - 1))
+                #coordinates used for the logs-slice
+                tl_l = (max(radius - x, 0)        , max(radius - y, 0))
+                br_l = (min(radius - x + width - 1, 2 * radius), min(radius - y + height - 1, 2 * radius))
+                
+                #extract the necessary parts of the arrays
+                logs_relevant = logs[tl_l[1]:br_l[1]+1, tl_l[0]:br_l[0]+1]
 
-                    if 0 <= pt[0] < height and 0 <= pt[1] < width:
-                        values[pt] += world.watermap['data'][y][x] / logs[it_logs.multi_index]
+                #finish calculation
+                values[tl_v[1]:br_v[1]+1, tl_v[0]:br_v[0]+1] += numpy.divide(world.watermap['data'][y][x], logs_relevant)
 
-                    it_logs.iternext()
             it_all.iternext()
 
         return values
