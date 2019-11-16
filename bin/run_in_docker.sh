f1c2d45c2c1d910a626892425ad3fa7086dc18f3
Nick Hynes <nhynes@nhynes.com>
2019-11-16 15:34:22 -0800

    Add gdal deps to docker

--- bin/run_in_docker.sh
+++ bin/run_in_docker.sh
@@ -1,15 +1,4 @@
 #! /bin/bash
 
-docker build . -t worldengine
-
-XPLAT_TMP_DIR=$(python -c "import tempfile; print(tempfile.gettempdir())")
-
-echo ''
-echo '---------------------------'
-echo 'Running worldengine'
-echo ''
-echo "PNGs will be output in the $XPLAT_TMP_DIR directory"
-echo '---------------------------'
-echo ''
-
-docker run -v $XPLAT_TMP_DIR:/tmp -it worldengine python worldengine -o /tmp
+docker build docker -t worldengine
+docker run --rm -v $(git rev-parse --show-toplevel):/opt/worldengine -it worldengine -o output $@
